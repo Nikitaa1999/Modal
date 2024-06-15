22bb1884e4dd8d118ec027faa5f7769894eb43dd
unknown <nikita.phulkar@gmail.com>
2024-06-16 00:50:01 +0530

    Initial Commit

---
+++ xmodal/src/App.js
@@ -0,0 +1,12 @@
+import ModalForm from "./ModalForm";
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+     <ModalForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ xmodal/src/ModalForm.jsx
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+import validator from 'validator' 
+
+
+Modal.setAppElement('#root');
+
+const ModalForm = () => {
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+  const [birthdate, setBirthdate] = useState('');
+
+
+  const openModal = () => setModalIsOpen(true);
+  const closeModal = () => setModalIsOpen(false);
+
+  const validateForm = () => {
+    let formErrors = {};
+    if (!name) formErrors.name = "Name is required";
+    if (!email) formErrors.email = "Email is required";
+    else if (!/\S+@\S+\.\S+/.test(email)) formErrors.email = "Email is invalid";
+    return formErrors;
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // const validationErrors = validateForm();
+    // if (Object.keys(validationErrors).length > 0) {
+    //   setErrors(validationErrors);
+    // } else {
+    //   console.log("Form Submitted:", { name, email });
+    //   closeModal();
+    // }
+    const isValidPhoneNumber = validator.isMobilePhone(phone);
+    console.log(isValidPhoneNumber)
+    if(!isValidPhoneNumber){
+        alert("Invalid phone number. Please enter a 10-digit phone number.");
+    }
+    else if(new Date(birthdate)>new Date()){
+        alert("Invalid date of birth. Date of birth cannot be in future.")
+    }
+    
+    else{
+        closeModal();
+    }
+
+  };
+
+  return (
+    <div>
+    <h1>User Details Modal</h1>
+      <button onClick={openModal}>Open Form</button>
+      <Modal isOpen={modalIsOpen} onRequestClose={closeModal} contentLabel="Example Modal">
+        <h2>Fill Details</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label>Username:</label>
+            <input
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              required
+            />
+            {/* {errors.name && <span>{errors.name}</span>} */}
+          </div>
+          <div>
+            <label>Email Address:</label>
+            <input
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+            {/* {errors.email && <span>{errors.email}</span>} */}
+          </div>
+          <div>
+            <label>Phone Number:</label>
+            <input
+              type="text"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              required
+            />
+            {/* {errors.email && <span>{errors.email}</span>} */}
+          </div>
+          <div>
+            <label>Birthdate:</label>
+            <input
+              type="date"
+              value={birthdate}
+              onChange={(e) => setBirthdate(e.target.value)}
+              required
+            />
+            {/* {errors.birthdate && <span>{errors.birthdate}</span>} */}
+          </div>
+          <button type="submit">Submit</button>
+          <button type="button" onClick={closeModal}>Close</button>
+        </form>
+      </Modal>
+    </div>
+  );
+};
+
+export default ModalForm;
